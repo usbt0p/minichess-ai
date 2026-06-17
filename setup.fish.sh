4fcac57bdeeb10edd6a4095a5c02bd9d5b490f4b
usbt0p <lucascdeaa@gmail.com>
2026-06-17 17:51:03 +0200

    update setup script

--- setup.fish.sh
+++ setup.fish.sh
@@ -1,4 +1,12 @@
-#!/usr/bin/env fish
-set --export PYTHONPATH /home/usbt0p/TFG
-echo $PYTHONPATH
-source .venv/bin/activate.fish
+# compatible setup script for Bash, Zsh, and Fish shell
+
+# this MUST be run on the project root for correct path export
+# so for example: cd /home/lucas/tfg/minichess-ai && source setup.sh 
+export PYTHONPATH="." 
+
+# Conditionally activate the virtual environment based on the shell
+[ -n "$BASH_VERSION" ] && source .venv/bin/activate
+[ -n "$ZSH_VERSION" ] && source .venv/bin/activate
+[ -n "$FISH_VERSION" ] && source .venv/bin/activate.fish
+
+true
