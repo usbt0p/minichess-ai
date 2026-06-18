prepare for ablations: ablation script, fix optuna, add seed and precision configs

src/experiment_scripts/run_optuna_tuning.py
@@ -54,11 +54,11 @@ class TuningObjective:
         # IMPORTANT!!
         lr = trial.suggest_float("lr", 4e-5, 1e-2, log=True)
         beta1 = trial.suggest_float("beta1", 0.85, 0.95)
-        weight_decay = trial.suggest_float("weight_decay", 1e-6, 1e-4, log=True)
+        weight_decay = trial.suggest_float("weight_decay", 1e-5, 0.1)
         #eps = trial.suggest_float("eps", 1e-9, 1e-6, log=True)
             
         # log folder for TensorBoard tracing
-        run_name = f"tuning/{self.study_name}_trial{trial.number}_lr{lr:.6f}_beta1{beta1:.4f}_eps{eps:.2e}"
+        run_name = f"tuning/{self.study_name}_trial{trial.number}_lr{lr:.6f}_beta1{beta1:.4f}_wd{weight_decay:.2e}"
         
         train_config = TrainingConfig(
             data_path=DATASET_PATH,
@@ -94,7 +94,7 @@ class TuningObjective:
         model = MiniChessTransformerEncoder(encoder_config)
         model = torch.compile(model)
  
-        print(f"\n  --> Trial {trial.number}: Testing LR = {lr:.6e}, Beta1 = {beta1:.4f}, Epsilon = {eps:.2e}")
+        print(f"\n  --> Trial {trial.number}: Testing LR = {lr:.6e}, Beta1 = {beta1:.4f}, Weight Decay = {weight_decay:.2e}")
         start_time = time.time()
         
         try:

src/training/train_transformer.py
@@ -373,7 +373,7 @@ def test_model_holdout(model, train_config):
 
 if __name__ == '__main__':
     args = parse_args()
-    set_seed(42)
+    set_seed(args.seed) # defaults to 42
     d_k = args.embed_dim
 
     # Initialize train configurations
@@ -419,8 +419,8 @@ if __name__ == '__main__':
         representation=train_config.representation,
         use_factorized_policy=train_config.use_factorized_policy,
     )
-    # TODO donde poner esto
-    torch.set_float32_matmul_precision('highest')
+    
+    torch.set_float32_matmul_precision(args.precision)
     
     print(encoder_config)
 